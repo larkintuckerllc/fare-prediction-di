1fb51556920ec7394d08ac68c5c89185dd13a816
John Tucker <jtucker@spotify.com>
2025-04-21 14:33:12 -0400

    iam

--- prepare.tf
+++ prepare.tf
@@ -1,26 +1,49 @@
-# resource "aws_iam_role" "fare_prediction_etl" {
-#   name = "AWSGlueServiceRole-fare-prediction-etl"
+resource "aws_iam_policy" "fare_prediction_etl_s3" {
+  name        = "fare-prediction-etl-s3"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Principal = {
-#           Service = "glue.amazonaws.com"
-#         }
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.fare_prediction.arn,
+          "${aws_s3_bucket.fare_prediction.arn}/*"
+        ]
+      }
+    ]
+  })
+}
 
-# resource "aws_iam_role_policy_attachment" "fare_prediction_etl_glue_service_role_policy" {
-#   role       = aws_iam_role.fare_prediction_etl.name
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
-# }
+resource "aws_iam_role" "fare_prediction_etl" {
+  name = "AWSGlueServiceRole-fare-prediction-etl"
 
-# resource "aws_iam_role_policy_attachment" "fare_prediction_etl_s3" {
-#   role       = aws_iam_role.fare_prediction_etl.name
-#   policy_arn = aws_iam_policy.fare_prediction_s3.arn
-# }
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "glue.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "fare_prediction_etl_glue_service" {
+  role       = aws_iam_role.fare_prediction_etl.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+resource "aws_iam_role_policy_attachment" "fare_prediction_etl_s3" {
+  role       = aws_iam_role.fare_prediction_etl.name
+  policy_arn = aws_iam_policy.fare_prediction_etl_s3.arn
+}
