4937f74d24b55cfb9a0a160a15e295059792c4ad
John Tucker <jtucker@spotify.com>
2025-04-21 21:16:56 -0400

    orch

---
+++ orchestrate.tf
@@ -0,0 +1,44 @@
+resource "aws_glue_workflow" "fare_prediction" {
+  name = "fare-prediction"
+}
+
+resource "aws_iam_policy" "fare_prediction_glue_notify_event" {
+  name        = "fare-prediction-glue-notify-event"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "glue:NotifyEvent"
+        ]
+        Resource = [
+          aws_glue_workflow.fare_prediction.arn
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "fare_prediction_eventbridge_glue_notify_event" {
+  name = "fare-prediction-eventbridge-glue-notify-event"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "events.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "fare_prediction_eventbridge_glue_notify_event" {
+  role       = aws_iam_role.fare_prediction_eventbridge_glue_notify_event.name
+  policy_arn = aws_iam_policy.fare_prediction_glue_notify_event.arn
+}
